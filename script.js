0e46e567962a5ffcef9a00a8a82002ce9352a951
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-29 18:37:16 +0100

    feat : adding response for password strength

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-document.querySelector(".strengthMeter").style.width = 0;
 function passwordValue() {
   const num = document.querySelector(".password").value.length;
+  const text = document.querySelector(".response");
 
   document.querySelector(".strengthMeter").style.width = num * 5 + "px";
-  progressBarColor(num * 5);
+  progressBarColor(num * 5, text);
 }
-function progressBarColor(num) {
+function progressBarColor(num, text) {
   if (num < 40) {
     document.querySelector(".strengthMeter").style.background = "red";
+    text.innerHTML = "Should be longer";
   } else if (num < 60) {
     document.querySelector(".strengthMeter").style.background = "orange";
+    text.innerHTML = "Pretty good";
   } else if (num > 60) {
     document.querySelector(".strengthMeter").style.background = "green";
+    text.innerHTML = "Grrrreat!";
   }
 }
