01c3c7aca86c25acab0a56e23fa4261c13f007e3
mousumi-ish <baral_mousumi@yahoo.com>
2023-08-29 18:00:07 +0100

    feat: progress bar increases and changes color depending on input characters

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
+document.querySelector(".strengthMeter").style.width = 0;
+function passwordValue() {
+  const num = document.querySelector(".password").value.length;
 
-
+  document.querySelector(".strengthMeter").style.width = num * 5 + "px";
+  progressBarColor(num * 5);
+}
+function progressBarColor(num) {
+  if (num < 40) {
+    document.querySelector(".strengthMeter").style.background = "red";
+  } else if (num < 60) {
+    document.querySelector(".strengthMeter").style.background = "orange";
+  } else if (num > 60) {
+    document.querySelector(".strengthMeter").style.background = "green";
+  }
+}
